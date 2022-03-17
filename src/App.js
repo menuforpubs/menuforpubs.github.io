2882675a09ac7259de4ee50d191876bbb2e2516e
danielmargolin <margolin23@gmail.com>
2022-03-17 14:41:39 +0200

    Auto refresh
    two minutes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+
 import './App.css';
 
 import Routes from './components/Routes/Routes';
 import MenuProvider from 'components/MenuProvider/MenuProvider';
 
 function App() {
+  useEffect(() => {
+    const timerToRefresh = setTimeout(() => {
+      window.location.reload();
+    }, 120000);
+
+    return () => clearTimeout(timerToRefresh);
+  }, []);
+
   return (
     <MenuProvider>
       <Routes />
